8a08aadd65153542d371cdd147b28728636d9ded
ChrissiWi <ChrissiWi@users.noreply.github.com>
2025-04-27 09:53:11 +0200

    add ButtonClicked

--- JapaneseTrainer/MainPage.xaml.cs
+++ JapaneseTrainer/MainPage.xaml.cs
@@ -7,5 +7,50 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
+	private void OnButtonAClicked(object sender, EventArgs e)
+	{
+		Button? button = sender as Button;
+		if (button != null)
+		{
+			button.BackgroundColor = Colors.Red;
+		}
+	}
+
+	private void OnButtonBClicked(object sender, EventArgs e)
+	{
+		Button? button = sender as Button;
+		if (button != null)
+		{
+			button.BackgroundColor = Colors.Red;
+		}
+	}
+
+	private void OnButtonCClicked(object sender, EventArgs e)
+	{
+		Button? button = sender as Button;
+		if (button != null)
+		{
+			button.BackgroundColor = Colors.Red;
+		}
+	}
+
+	private void OnButtonDClicked(object sender, EventArgs e)
+	{
+		Button? button = sender as Button;
+		if (button != null)
+		{
+			button.BackgroundColor = Colors.Red;
+		}
+	}
+
+	private void OnButtonNextClicked(object sender, EventArgs e)
+	{
+		Button? button = sender as Button;
+		if (button != null)
+		{
+			button.BackgroundColor = Colors.Green;
+		}
+	}
+
 }
 
