02f98ea868511d242b23b9a1c19d744d593e8d71
ChrissiWi <ChrissiWi@users.noreply.github.com>
2025-04-27 10:22:58 +0200

    show 4 different answers

--- JapaneseTrainer/MainPage.xaml.cs
+++ JapaneseTrainer/MainPage.xaml.cs
@@ -21,6 +21,16 @@ public partial class MainPage : ContentPage, INotifyPropertyChanged
 		}
 	}
 
+	public string AnswerA 
+	{ 
+		get; 
+		set; 
+	} = string.Empty;
+
+	public string AnswerB { get; set; } = string.Empty;
+	public string AnswerC { get; set; } = string.Empty;
+	public string AnswerD { get; set; } = string.Empty;
+
 	public MainPage()
 	{
 		InitializeComponent();
@@ -32,8 +42,36 @@ public partial class MainPage : ContentPage, INotifyPropertyChanged
 		{
 			_vocabularies = ReadVocabulariesFromDB(MauiProgram.DatabasePath);
 			CurrentVocabulary = _vocabularies[Random.Shared.Next(_vocabularies.Count)];
+			SetAnswers();
 		}
 		BindingContext = this;
+	}
+
+	private void SetAnswers()
+	{
+		AnswerA = _vocabularies[Random.Shared.Next(_vocabularies.Count)].Translation;
+		AnswerB = _vocabularies[Random.Shared.Next(_vocabularies.Count)].Translation;
+		AnswerC = _vocabularies[Random.Shared.Next(_vocabularies.Count)].Translation;
+		AnswerD = _vocabularies[Random.Shared.Next(_vocabularies.Count)].Translation;
+		
+		if (CurrentVocabulary != null)
+		{
+			switch (Random.Shared.Next(4))
+			{
+				case 0:
+					AnswerA = CurrentVocabulary.Translation;
+					break;
+				case 1:
+					AnswerB = CurrentVocabulary.Translation;
+					break;
+				case 2:
+					AnswerC = CurrentVocabulary.Translation;
+					break;
+				case 3:
+					AnswerD = CurrentVocabulary.Translation;
+						break;
+			}
+		}
 	}
 
 	private List<Vocabulary> ReadVocabulariesFromDB(string databasePath)
