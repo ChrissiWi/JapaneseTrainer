71001ab9c7529f15f8ea11e76f61780734f0e1d2
ChrissiWi <ChrissiWi@users.noreply.github.com>
2025-04-27 10:05:51 +0200

    show word and translation

--- JapaneseTrainer/MainPage.xaml.cs
+++ JapaneseTrainer/MainPage.xaml.cs
@@ -1,10 +1,36 @@
-﻿namespace JapaneseTrainer;
+﻿using JapaneseTrainer.Data;
+using JapaneseTrainer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JapaneseTrainer;
 
 public partial class MainPage : ContentPage
 {
+
+	private Vocabulary _currentVocabulary;
+	private List<Vocabulary> _vocabularies;
+
 	public MainPage()
 	{
 		InitializeComponent();
+		if (MauiProgram.DatabasePath == null)
+		{
+			throw new Exception("Database path is null");
+		}
+		else
+		{
+			_vocabularies = ReadVocabulariesFromDB(MauiProgram.DatabasePath);
+			_currentVocabulary = _vocabularies[Random.Shared.Next(_vocabularies.Count)];
+		}
+	}
+
+	private List<Vocabulary> ReadVocabulariesFromDB(string databasePath)
+	{
+		var options = new DbContextOptionsBuilder<JapaneseTrainerContext>()
+			.UseSqlite($"Data Source={databasePath}")
+			.Options;
+		using var context = new JapaneseTrainerContext(options);
+		return context.Vocabularies.ToList();
 	}
 
 	private void OnButtonAClicked(object sender, EventArgs e)
