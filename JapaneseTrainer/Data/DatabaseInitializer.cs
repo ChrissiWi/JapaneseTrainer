c0b9224d852db6c7b0ec12f4373e1ad74309b64e
ChrissiWi <ChrissiWi@users.noreply.github.com>
2025-04-28 19:09:54 +0200

    add katakana

--- JapaneseTrainer/Data/DatabaseInitializer.cs
+++ JapaneseTrainer/Data/DatabaseInitializer.cs
@@ -22,6 +22,7 @@ public static class DatabaseInitializer
             new Vocabulary { JapaneseWord = "はい", Translation = "Yes" },
             new Vocabulary { JapaneseWord = "いいえ", Translation = "No" },
             new Vocabulary { JapaneseWord = "おはよう", Translation = "Good morning" },
+            new Vocabulary { JapaneseWord = "おはようございます", Translation = "Good morning (formal)" },
             new Vocabulary { JapaneseWord = "こんばんは", Translation = "Good evening" },
             new Vocabulary { JapaneseWord = "さようなら", Translation = "Goodbye" },
             new Vocabulary { JapaneseWord = "お元気ですか", Translation = "How are you?" },
@@ -44,11 +45,19 @@ public static class DatabaseInitializer
             new Vocabulary { JapaneseWord = "犬", Translation = "Dog" },
             new Vocabulary { JapaneseWord = "雑誌", Translation = "Magazine" },
             new Vocabulary { JapaneseWord = "机", Translation = "Desk" },
-            new Vocabulary { JapaneseWord = "日本語", Translation = "Japanese (language)" }
+            new Vocabulary { JapaneseWord = "日本語", Translation = "Japanese (language)" },
+            
+            // Additional vocabulary words
+            new Vocabulary { JapaneseWord = "野菜", Translation = "Vegetables" },
+            new Vocabulary { JapaneseWord = "魚", Translation = "Fish" },
+            new Vocabulary { JapaneseWord = "卵", Translation = "Egg" },
+            new Vocabulary { JapaneseWord = "家族", Translation = "Family" },
+            new Vocabulary { JapaneseWord = "空", Translation = "Sky" }
         });
 
-        // Add hiragana characters
+        // Add hiragana and katakana characters
         vocabularies.AddRange(GetHiraganaCharacters());
+        vocabularies.AddRange(GetKatakanaCharacters());
 
         context.Vocabularies.AddRange(vocabularies);
         context.SaveChanges();
@@ -168,6 +177,123 @@ public static class DatabaseInitializer
             new Vocabulary { JapaneseWord = "ゃ", Translation = "ya" },
             new Vocabulary { JapaneseWord = "ゅ", Translation = "yu" },
             new Vocabulary { JapaneseWord = "ょ", Translation = "yo" }
+        };
+    }
+
+    private static List<Vocabulary> GetKatakanaCharacters()
+    {
+        return new List<Vocabulary>
+        {
+            // Basic vowels
+            new Vocabulary { JapaneseWord = "ア", Translation = "a" },
+            new Vocabulary { JapaneseWord = "イ", Translation = "i" },
+            new Vocabulary { JapaneseWord = "ウ", Translation = "u" },
+            new Vocabulary { JapaneseWord = "エ", Translation = "e" },
+            new Vocabulary { JapaneseWord = "オ", Translation = "o" },
+
+            // K-column
+            new Vocabulary { JapaneseWord = "カ", Translation = "ka" },
+            new Vocabulary { JapaneseWord = "キ", Translation = "ki" },
+            new Vocabulary { JapaneseWord = "ク", Translation = "ku" },
+            new Vocabulary { JapaneseWord = "ケ", Translation = "ke" },
+            new Vocabulary { JapaneseWord = "コ", Translation = "ko" },
+
+            // S-column
+            new Vocabulary { JapaneseWord = "サ", Translation = "sa" },
+            new Vocabulary { JapaneseWord = "シ", Translation = "shi" },
+            new Vocabulary { JapaneseWord = "ス", Translation = "su" },
+            new Vocabulary { JapaneseWord = "セ", Translation = "se" },
+            new Vocabulary { JapaneseWord = "ソ", Translation = "so" },
+
+            // T-column
+            new Vocabulary { JapaneseWord = "タ", Translation = "ta" },
+            new Vocabulary { JapaneseWord = "チ", Translation = "chi" },
+            new Vocabulary { JapaneseWord = "ツ", Translation = "tsu" },
+            new Vocabulary { JapaneseWord = "テ", Translation = "te" },
+            new Vocabulary { JapaneseWord = "ト", Translation = "to" },
+
+            // N-column
+            new Vocabulary { JapaneseWord = "ナ", Translation = "na" },
+            new Vocabulary { JapaneseWord = "ニ", Translation = "ni" },
+            new Vocabulary { JapaneseWord = "ヌ", Translation = "nu" },
+            new Vocabulary { JapaneseWord = "ネ", Translation = "ne" },
+            new Vocabulary { JapaneseWord = "ノ", Translation = "no" },
+
+            // H-column
+            new Vocabulary { JapaneseWord = "ハ", Translation = "ha" },
+            new Vocabulary { JapaneseWord = "ヒ", Translation = "hi" },
+            new Vocabulary { JapaneseWord = "フ", Translation = "fu" },
+            new Vocabulary { JapaneseWord = "ヘ", Translation = "he" },
+            new Vocabulary { JapaneseWord = "ホ", Translation = "ho" },
+
+            // M-column
+            new Vocabulary { JapaneseWord = "マ", Translation = "ma" },
+            new Vocabulary { JapaneseWord = "ミ", Translation = "mi" },
+            new Vocabulary { JapaneseWord = "ム", Translation = "mu" },
+            new Vocabulary { JapaneseWord = "メ", Translation = "me" },
+            new Vocabulary { JapaneseWord = "モ", Translation = "mo" },
+
+            // Y-column
+            new Vocabulary { JapaneseWord = "ヤ", Translation = "ya" },
+            new Vocabulary { JapaneseWord = "ユ", Translation = "yu" },
+            new Vocabulary { JapaneseWord = "ヨ", Translation = "yo" },
+
+            // R-column
+            new Vocabulary { JapaneseWord = "ラ", Translation = "ra" },
+            new Vocabulary { JapaneseWord = "リ", Translation = "ri" },
+            new Vocabulary { JapaneseWord = "ル", Translation = "ru" },
+            new Vocabulary { JapaneseWord = "レ", Translation = "re" },
+            new Vocabulary { JapaneseWord = "ロ", Translation = "ro" },
+
+            // W-column
+            new Vocabulary { JapaneseWord = "ワ", Translation = "wa" },
+            new Vocabulary { JapaneseWord = "ヲ", Translation = "wo" },
+
+            // N
+            new Vocabulary { JapaneseWord = "ン", Translation = "n" },
+
+            // Dakuten (voiced sounds)
+            new Vocabulary { JapaneseWord = "ガ", Translation = "ga" },
+            new Vocabulary { JapaneseWord = "ギ", Translation = "gi" },
+            new Vocabulary { JapaneseWord = "グ", Translation = "gu" },
+            new Vocabulary { JapaneseWord = "ゲ", Translation = "ge" },
+            new Vocabulary { JapaneseWord = "ゴ", Translation = "go" },
+
+            new Vocabulary { JapaneseWord = "ザ", Translation = "za" },
+            new Vocabulary { JapaneseWord = "ジ", Translation = "ji" },
+            new Vocabulary { JapaneseWord = "ズ", Translation = "zu" },
+            new Vocabulary { JapaneseWord = "ゼ", Translation = "ze" },
+            new Vocabulary { JapaneseWord = "ゾ", Translation = "zo" },
+
+            new Vocabulary { JapaneseWord = "ダ", Translation = "da" },
+            new Vocabulary { JapaneseWord = "ヂ", Translation = "ji" },
+            new Vocabulary { JapaneseWord = "ヅ", Translation = "zu" },
+            new Vocabulary { JapaneseWord = "デ", Translation = "de" },
+            new Vocabulary { JapaneseWord = "ド", Translation = "do" },
+
+            new Vocabulary { JapaneseWord = "バ", Translation = "ba" },
+            new Vocabulary { JapaneseWord = "ビ", Translation = "bi" },
+            new Vocabulary { JapaneseWord = "ブ", Translation = "bu" },
+            new Vocabulary { JapaneseWord = "ベ", Translation = "be" },
+            new Vocabulary { JapaneseWord = "ボ", Translation = "bo" },
+
+            // Handakuten (semi-voiced sounds)
+            new Vocabulary { JapaneseWord = "パ", Translation = "pa" },
+            new Vocabulary { JapaneseWord = "ピ", Translation = "pi" },
+            new Vocabulary { JapaneseWord = "プ", Translation = "pu" },
+            new Vocabulary { JapaneseWord = "ペ", Translation = "pe" },
+            new Vocabulary { JapaneseWord = "ポ", Translation = "po" },
+
+            // Small characters
+            new Vocabulary { JapaneseWord = "ァ", Translation = "a" },
+            new Vocabulary { JapaneseWord = "ィ", Translation = "i" },
+            new Vocabulary { JapaneseWord = "ゥ", Translation = "u" },
+            new Vocabulary { JapaneseWord = "ェ", Translation = "e" },
+            new Vocabulary { JapaneseWord = "ォ", Translation = "o" },
+            new Vocabulary { JapaneseWord = "ッ", Translation = "tsu" },
+            new Vocabulary { JapaneseWord = "ャ", Translation = "ya" },
+            new Vocabulary { JapaneseWord = "ュ", Translation = "yu" },
+            new Vocabulary { JapaneseWord = "ョ", Translation = "yo" }
         };
     }
 } 
